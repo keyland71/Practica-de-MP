b3d3a3d536ecd5daff75c235321f47d0652d7705
Lucia <l.dominguez.2021@alumnos.urjc.es>
2023-04-12 00:10:43 +0200

    iniciar clase numeroRegistro, implementar enumerados
    se iniciará la codificación de la clase numeroRegistro (casi completada y compila, falta revisar algunas funciones).
    
    Se ha implementado los enumerados necesarios de las clases del juego

--- Fase 3 Codificacion/PracticaMP/src/clasesDeJuego/NumeroRegistro.java
+++ Fase 3 Codificacion/PracticaMP/src/clasesDeJuego/NumeroRegistro.java
@@ -15,4 +15,21 @@ public class NumeroRegistro {
     private char caracter2;
     private char caracter3;
     
+    
+     public NumeroRegistro(char caracter1, int numero1, int numero2, char caracter2, char caracter3) {
+        this.caracter1 = caracter1;
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.caracter2 = caracter2;
+        this.caracter3 = caracter3;
+    }
+    public NumeroRegistro obtenerNumReg() {
+        return new NumeroRegistro(caracter1, numero1, numero2, caracter2, caracter3);
+    }
+    
+     public NumeroRegistro incrementarNumReg() {
+        int nuevoNumero1 = this.numero1 + 1;
+        return new NumeroRegistro(this.caracter1, nuevoNumero1, this.numero2, this.caracter2, this.caracter3);
+    }
+    
 }
