bb0510e3d44abcb4363857f0b4c02eca6fc1e815
Sergio de Oro Fernández <sergiodeoro422003@gmail.com>
2023-04-11 02:59:43 +0200

    Crear bucle inicial del juego
    Se muestra el menú de inicio con sus opciones y errores. Solo está implementada la opción de salir.
    Se añade un Scanner al menú para no tener que crearlo cada vez que se quiera leer una entrada.

--- Fase 3 Codificacion/PracticaMP/src/controladores/ControladorInicio.java
+++ Fase 3 Codificacion/PracticaMP/src/controladores/ControladorInicio.java
@@ -12,9 +12,52 @@ import menus.MenuInicio;
 public class ControladorInicio {
 
     private MenuInicio menuInicio;
-    
+    private int modo;
+
+    public ControladorInicio() {
+        this.menuInicio = new MenuInicio();
+        this.modo = 0;
+    }
+
     public void iniciarControlador() {
+        boolean salir = false;
+        while (!salir) {
+            String opcion = this.menuInicio.mostrarMensaje(this.modo);
+            boolean valido = validarEntrada(opcion);
+            if (valido) {
+                salir = procesarEntrada(opcion);
+            } else {
+                this.menuInicio.mostrarMensajeError(this.modo);
+            }
+        }
+    }
+
+    private boolean validarEntrada(String opcion) {
+        switch (this.modo) {
+            case 0 -> {
+                return opcion.equals("1") || opcion.equals("2") || opcion.equals("3");
+            }
+        }
+        return false;
         
     }
-    
+
+    private boolean procesarEntrada(String opcion) {
+        switch (this.modo) {
+            case 0 -> {
+                switch (opcion) {
+                    case "1" ->
+                        System.out.println("Iniciar sesión"); // Está a modo de placeholder, aún no está terminado
+                    case "2" ->
+                        System.out.println("Registrarse"); // Está a modo de placeholder, aún no está terminado
+                    default -> {
+                        System.out.println("Adiós");
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 }
--- Fase 3 Codificacion/PracticaMP/src/menus/MenuInicio.java
+++ Fase 3 Codificacion/PracticaMP/src/menus/MenuInicio.java
@@ -1,6 +1,8 @@
 package menus;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -10,8 +12,37 @@ import java.util.List;
  * @author Marcos Jiménez Pulido
  */
 public class MenuInicio {
-    
+
     private List<String> mensajes;
     private List<String> mensajesError;
-    
+    private Scanner lector;
+
+    public MenuInicio() {
+        this.mensajes = new ArrayList<>();
+        this.mensajesError = new ArrayList<>();
+        String mensaje = """
+                         Bienvenido a MP (Majestuosas Peleas).
+                         Por favor, elija una opción:
+                         1) Iniciar sesión
+                         2) Registrarse
+                         3) Salir""";
+        this.mensajes.add(mensaje);
+        mensaje = "Por favor, introduzca un valor correcto. Pulse cualquier botón para continuar";
+        this.mensajesError.add(mensaje);
+        this.lector = new Scanner(System.in);
+    }
+
+    public String mostrarMensaje(int pos) {
+        String mensaje = this.mensajes.get(pos);
+        System.out.println(mensaje);
+        String opcion = this.lector.nextLine();
+        return opcion;
+    }
+
+    public void mostrarMensajeError(int modo) {
+        String mensaje = this.mensajesError.get(modo);
+        System.out.println(mensaje);
+        (new Scanner(System.in)).nextLine();
+    }
+
 }
